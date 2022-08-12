458557a4c9605bf0a568c05ead21155088544313
linchengdong <chengdong2518@163.com>
2022-08-12 14:33:36 +0800

    基于 MessageToMessageEncoder优化发送逻辑

---
+++ client/src/main/java/com/unic/client/ClientV1.java
@@ -0,0 +1,79 @@
+package com.unic.client;
+
+import com.unic.client.codec.*;
+import com.unic.core.base.RequestMessage;
+import com.unic.core.order.OrderOperation;
+import com.unic.core.util.IdUtil;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioChannelOption;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+import javax.net.ssl.SSLException;
+import java.util.concurrent.ExecutionException;
+
+public class ClientV1 {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException, SSLException {
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.channel(NioSocketChannel.class);
+
+        bootstrap.option(NioChannelOption.CONNECT_TIMEOUT_MILLIS, 10 * 1000);
+
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+
+            bootstrap.group(group);
+
+            final LoggingHandler loggingHandler = new LoggingHandler(LogLevel.INFO);
+
+            bootstrap.handler(new ChannelInitializer<NioSocketChannel>() {
+                @Override
+                protected void initChannel(NioSocketChannel ch) throws Exception {
+                    ChannelPipeline pipeline = ch.pipeline();
+
+                    pipeline.addLast(new OrderFrameDecoder());
+                    pipeline.addLast(new OrderFrameEncoder());
+
+                    pipeline.addLast(new OrderProtocolEncoder());
+                    pipeline.addLast(new OrderProtocolDecoder());
+
+                    pipeline.addLast(new OperationToRequestMessageEncoder());
+
+                    pipeline.addLast(loggingHandler);
+
+
+                }
+            });
+
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6668);
+
+            channelFuture.sync();
+
+
+            //channelFuture.channel().writeAndFlush(new RequestMessage(IdUtil.nextId(), authOperation));
+
+            // 以下两种方式都可以发送
+            // RequestMessage requestMessage = new RequestMessage(IdUtil.nextId(), new OrderOperation(1001, "tudou"));
+            OrderOperation operation = new OrderOperation(10020,"milk");
+
+            channelFuture.channel().writeAndFlush(operation);
+
+            channelFuture.channel().closeFuture().sync();
+
+        } finally{
+            group.shutdownGracefully();
+        }
+
+    }
+
+}
